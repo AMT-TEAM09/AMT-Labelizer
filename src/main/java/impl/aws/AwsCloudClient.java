62fd8aa449aa0436de51f941b7865bfbb4bc72f6
Stéphane Marengo <stephane.marengo@hotmail.com>
2022-11-01 13:30:12 +0100

    Implémentation de AwsCloudClient

--- src/main/java/impl/aws/AwsCloudClient.java
+++ src/main/java/impl/aws/AwsCloudClient.java
@@ -5,13 +5,32 @@ import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 
 public class AwsCloudClient implements CloudClient {
     private static final String BUCKET_NAME = "amt.team09.diduno.education";
+    private static final ProfileCredentialsProvider CREDENTIALS_PROVIDER = ProfileCredentialsProvider.create();
+
+    private static AwsCloudClient instance;
+
+    private AwsDataObjectHelper dataObjectHelper;
+    private AwsLabelDetector labelDetector;
 
     private AwsCloudClient() {
-        var credentialsProvider = ProfileCredentialsProvider.create();
+        dataObjectHelper = new AwsDataObjectHelper(CREDENTIALS_PROVIDER, BUCKET_NAME);
+        labelDetector = new AwsLabelDetector(CREDENTIALS_PROVIDER);
+    }
+
+    public static AwsCloudClient getInstance() {
+        if (instance == null) {
+            instance = new AwsCloudClient();
+        }
+        return instance;
     }
 
     @Override
-    public CloudClient getInstance() {
-        return null;
+    public AwsDataObjectHelper getDataObjectHelper() {
+        return dataObjectHelper;
+    }
+
+    @Override
+    public AwsLabelDetector getLabelDetector() {
+        return labelDetector;
     }
 }
