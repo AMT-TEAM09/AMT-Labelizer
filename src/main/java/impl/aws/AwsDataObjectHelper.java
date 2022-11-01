a9537877be58b8f3ea838e476b7da754f9531590
Stéphane Marengo <stephane.marengo@hotmail.com>
2022-11-01 13:30:28 +0100

    Implémentation de AwsDataObjectHelper

--- src/main/java/impl/aws/AwsDataObjectHelper.java
+++ src/main/java/impl/aws/AwsDataObjectHelper.java
@@ -2,19 +2,27 @@ package impl.aws;
 
 import interfaces.DataObjectHelper;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 public class AwsDataObjectHelper implements DataObjectHelper {
-    private S3Client client;
+    private final String bucketName;
+    private final S3Client client;
 
     public AwsDataObjectHelper(ProfileCredentialsProvider credentialsProvider, String bucketName) {
         client = S3Client.builder()
                 .credentialsProvider(credentialsProvider)
                 .build();
+        this.bucketName = bucketName;
     }
 
     @Override
     public void Create(String objectName) {
-//        client.getB;
+        var request = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(objectName)
+                .build();
+        client.putObject(request, RequestBody.empty());
     }
 }
