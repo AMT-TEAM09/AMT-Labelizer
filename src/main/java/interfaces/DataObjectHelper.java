730e6fadc8232b6e3565a18df028b15d8a9dd3cb
Stéphane Marengo <stephane.marengo@hotmail.com>
2022-11-03 10:24:38 +0100

    Réécriture des tests en given/when/then Ajout de publish dans l'interface

--- src/main/java/interfaces/DataObjectHelper.java
+++ src/main/java/interfaces/DataObjectHelper.java
@@ -1,5 +1,19 @@
 package interfaces;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Path;
+
 public interface DataObjectHelper {
-    void Create(String objectName);
+    void create(String objectName);
+
+    void create(String objectName, Path filePath);
+
+    boolean exists(String objectName);
+
+    void delete(String objectName);
+
+    URL publish(String objectName);
+
+    InputStream get(String objectName);
 }
--- src/test/java/impl/aws/AwsDataObjectHelperTest.java
+++ src/test/java/impl/aws/AwsDataObjectHelperTest.java
@@ -1,29 +1,157 @@
 package impl.aws;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class AwsDataObjectHelperTest {
-    private static final String BUCKET_NAME = "amt.team09.diduno.education";
-    private ProfileCredentialsProvider credentialsProvider;
+    private static final Path RESOURCE_PATH = Paths.get("src", "test", "resources");
+    private static final Path IMAGE_FILE = RESOURCE_PATH.resolve("image.jpg");
+    private static final String TEST_OBJECT_NAME = "test-object";
     private AwsDataObjectHelper objectHelper;
+    private String bucketName;
 
     @BeforeEach
     void setUp() {
-        credentialsProvider = ProfileCredentialsProvider.create();
-        objectHelper = new AwsDataObjectHelper(credentialsProvider, BUCKET_NAME);
+        Dotenv dotenv = Dotenv.configure().load();
+
+        var profile = dotenv.get("AWS_PROFILE");
+        var region = Region.of(dotenv.get("AWS_REGION"));
+        var credentialsProvider = ProfileCredentialsProvider.create(profile);
+        bucketName = dotenv.get("AWS_BUCKET_NAME");
+
+        objectHelper = new AwsDataObjectHelper(credentialsProvider, bucketName, region);
     }
 
     @AfterEach
     void tearDown() {
-        credentialsProvider.close();
+        if (objectHelper.exists(TEST_OBJECT_NAME)) {
+            objectHelper.delete(TEST_OBJECT_NAME);
+        }
     }
 
     @Test
-    void itShouldCreateAFile() {
-        objectHelper.Create("test");
+    void create_bucketExists_success() {
+        // given
+        assertTrue(objectHelper.exists(bucketName));
+        assertFalse(objectHelper.exists(TEST_OBJECT_NAME));
+
+        // when
+        objectHelper.create(TEST_OBJECT_NAME, IMAGE_FILE);
+
+        // then
+        assertTrue(objectHelper.exists(TEST_OBJECT_NAME));
+    }
+
+    @Test
+    void get_nominalCase_success() throws IOException {
+        // given
+        String fileContent;
+        try (var stream = Files.newInputStream(IMAGE_FILE)) {
+            fileContent = new String(stream.readAllBytes());
+        }
+        objectHelper.create(TEST_OBJECT_NAME, IMAGE_FILE);
+        assertTrue(objectHelper.exists(TEST_OBJECT_NAME));
+
+        // when
+        String downloadedContent;
+        try (var downloaded = objectHelper.get(TEST_OBJECT_NAME)) {
+            downloadedContent = new String(downloaded.readAllBytes());
+        }
+
+        // then
+        assertEquals(fileContent, downloadedContent);
+    }
+
+    @Test
+    void exists_nominalCase_success() {
+        // given
+        assertFalse(objectHelper.exists(TEST_OBJECT_NAME));
+        objectHelper.create(TEST_OBJECT_NAME, IMAGE_FILE);
+
+        // when
+        var exists = objectHelper.exists(TEST_OBJECT_NAME);
+
+        // then
+        assertTrue(exists);
+    }
+
+    @Test
+    void exists_objectNotExists_success() {
+        // given
+        assertFalse(objectHelper.exists(TEST_OBJECT_NAME));
+
+        // when
+        var exists = objectHelper.exists(TEST_OBJECT_NAME);
+
+        // then
+        assertFalse(exists);
+    }
+
+    @Test
+    void delete_objectNotExists_success() {
+        // given
+        assertFalse(objectHelper.exists(TEST_OBJECT_NAME));
+
+        // when
+        objectHelper.delete(TEST_OBJECT_NAME);
+
+        // then
+        assertFalse(objectHelper.exists(TEST_OBJECT_NAME));
+    }
+
+    @Test
+    void delete_objectExists_success() {
+        // given
+        objectHelper.create(TEST_OBJECT_NAME, IMAGE_FILE);
+        assertTrue(objectHelper.exists(TEST_OBJECT_NAME));
+
+        // when
+        objectHelper.delete(TEST_OBJECT_NAME);
+
+        // then
+        assertFalse(objectHelper.exists(TEST_OBJECT_NAME));
+    }
+
+    @Test
+    void publish_objectExists_success() throws IOException {
+        // given
+        objectHelper.create(TEST_OBJECT_NAME, IMAGE_FILE);
+        assertTrue(objectHelper.exists(TEST_OBJECT_NAME));
+
+        // when
+        objectHelper.publish(TEST_OBJECT_NAME);
+        var url = objectHelper.publish(TEST_OBJECT_NAME);
+
+        // then
+        var huc = (HttpURLConnection) url.openConnection();
+        huc.setRequestMethod("HEAD");
+        var responseCode = huc.getResponseCode();
+
+        assertEquals(200, responseCode);
+    }
+
+    @Test
+    void publish_objectNotExists_success() {
+        // given
+        assertFalse(objectHelper.exists(TEST_OBJECT_NAME));
+
+        // when
+        var url = objectHelper.publish(TEST_OBJECT_NAME);
+
+        // then
+        assertNull(url);
     }
 }
