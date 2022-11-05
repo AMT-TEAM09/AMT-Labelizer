0af6c8f1f2f9153b76ec443d7bc63749917bafd7
Stéphane Marengo <stephane.marengo@hotmail.com>
2022-11-05 23:37:54 +0100

    Suppression de la méthode toJson de Label

--- src/main/java/models/Label.java
+++ src/main/java/models/Label.java
@@ -1,16 +1,4 @@
 package models;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public record Label(String name, float confidence) {
-    public String toJson() {
-        var mapper = new ObjectMapper();
-        try {
-            return mapper.writeValueAsString(this);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            return "{}";
-        }
-    }
 }
